6acd1c981869514f8f9ef01cbbbf51ae6d4c1940
Steffen <steffen.schotthoefer@outlook.de>
2024-04-15 16:34:13 -0400

    update client lattice script

--- client_lattice.py
+++ client_lattice.py
@@ -7,8 +7,8 @@ model = umbridge.HTTPModel(url, "forward")
 # design parameter vector is 2d (scatter_white, absorption_blue)
 
 parameter_range_n_cell =[30]#,30,40,50, 60, 80, 160] #[10, 20, 40] # 10 means 10^2 cells per lattice square. Cell size reduces with geometric progression (1.05) towards square boundary
-parameter_range_quad_order =[10]# 20 , 30 ,40 ,50 ]       # LDFESA quadrature
-parameter_range_abs_blue = [  10, 20]#, 50, 100]  # Prescribed range for LATTICE_DSGN_ABSORPTION_BLUE
+parameter_range_quad_order =[10]# 20 , 30 ,40 ,50 ]  # Gauss Legendre  quadrature
+parameter_range_abs_blue = [10, 20]#, 50, 100]  # Prescribed range for LATTICE_DSGN_ABSORPTION_BLUE
 parameter_range_scatter_white = [ 1, 2, 5]#, 10]  # Prescribed range for LATTICE_DSGN_ABSORPTION_BLUE
 
 design_params = []
