fixed postprocessing for time averaged hohlraum scripts

client_half_lattice.py
@@ -1,5 +1,6 @@
-import umbridge
+# import umbridge
 import numpy as np
+import os
 
 from src.config_utils import read_username_from_config
 from src.simulation_utils import execute_slurm_scripts, wait_for_slurm_jobs
@@ -8,9 +9,22 @@ from src.general_utils import (
     load_hohlraum_samples_from_npz,
     delete_slurm_scripts,
 )
+from src.config_utils import (
+    generate_log_filename,
+    read_config_file,
+    update_parameter,
+    write_config_file,
+    remove_files,
+    update_half_lattice_mesh_file,
+    write_slurm_file,
+)
+from src.scraping_utils import read_csv_file
+from src.simulation_utils import run_cpp_simulation_containerized
+from src.general_utils import replace_next_line
 
-url = "http://localhost:4243"
-model = umbridge.HTTPModel(url, "forward")
+
+# url = "http://localhost:4243"
+# model = umbridge.HTTPModel(url, "forward")
 
 
 def main():
@@ -20,7 +34,7 @@ def main():
     # Define parameter ranges
     # characteristic length of the cells
     parameter_range_n_cell = [
-        0.01,
+        0.02,
     ]
     #    0.0075,
     #    0.005,
@@ -106,6 +120,108 @@ def call_models(
         qois.append(res[0])
 
     return np.array(qois)
+
+
+def model(parameters):
+    """
+    A function that performs a series of steps including reading a base config file,
+    updating parameters based on input values, running a C++ simulation, and processing
+    the output data. It takes in parameters and a config dictionary, and returns a list
+    of quantities of interest calculated during the simulation.
+    """
+
+    scatter_white_value = parameters[0][0]
+    absorption_blue_value = parameters[0][1]
+
+    n_cells = parameters[0][2]
+    quad_order = int(parameters[0][3])
+
+    hpc_operation = parameters[0][4]
+
+    subfolder = "benchmarks/half_lattice/"
+    base_config_file = subfolder + "half_lattice.cfg"
+
+    # Step 1: Read the base config file
+    kitrt_parameters = read_config_file(base_config_file)
+    lattice_file_new = update_half_lattice_mesh_file(n_cells, subfolder + "mesh/")
+
+    unique_name = f"half_lattice_abs{absorption_blue_value}_scatter{scatter_white_value}_p{n_cells}_q{quad_order}"
+    if hpc_operation == 2:
+        if os.path.exists(
+            subfolder + "mesh/" + "hohlraum_variable_backup" + unique_name + ".geo"
+        ):
+            os.remove(
+                subfolder + "mesh/" + "hohlraum_variable_backup" + unique_name + ".geo"
+            )  # remove backup geo files
+
+    # Step 2: Update kitrt_parameters for the current value of LATTICE_DSGN_ABSORPTION_BLUE
+    kitrt_parameters = update_parameter(
+        kitrt_parameters,
+        key="LATTICE_DSGN_ABSORPTION_BLUE",
+        new_value=absorption_blue_value,
+    )
+    kitrt_parameters = update_parameter(
+        kitrt_parameters,
+        key="LATTICE_DSGN_SCATTER_WHITE",
+        new_value=scatter_white_value,
+    )
+    kitrt_parameters = update_parameter(
+        kitrt_parameters, key="QUAD_ORDER", new_value=quad_order
+    )
+    kitrt_parameters = update_parameter(
+        kitrt_parameters, key="MESH_FILE", new_value="mesh/" + lattice_file_new
+    )
+
+    # Step 3: Update LOG_FILE to a unique identifier linked to LATTICE_DSGN_ABSORPTION_BLUE
+
+    kitrt_parameters = update_parameter(
+        kitrt_parameters, key="LOG_FILE", new_value=unique_name
+    )
+    if hpc_operation < 2:
+        remove_files(subfolder + kitrt_parameters["LOG_DIR"] + "/" + unique_name)
+    kitrt_parameters = update_parameter(
+        kitrt_parameters, key="OUTPUT_FILE", new_value=unique_name
+    )
+    if hpc_operation < 2:
+        remove_files(
+            subfolder + kitrt_parameters["OUTPUT_DIR"] + "/" + unique_name + ".vtk"
+        )
+
+    # Step 4: Write a new config file, named corresponding to LATTICE_DSGN_ABSORPTION_BLUE
+    generated_cfg_file = subfolder + unique_name + ".cfg"
+    write_config_file(parameters=kitrt_parameters, output_file_path=generated_cfg_file)
+
+    # Step 5: Run the C++ simulation
+    if hpc_operation == 0:
+        # Step 5: Run the C++ simulation
+        run_cpp_simulation_containerized(generated_cfg_file)
+    elif hpc_operation == 1:
+        # Write slurm file
+        write_slurm_file(
+            "benchmarks/half_lattice/slurm_scripts/", unique_name, subfolder
+        )
+
+    # Step 6: Read the log file
+    if hpc_operation == 0 or hpc_operation == 2:
+        log_filename = generate_log_filename(kitrt_parameters)
+
+        if log_filename:
+            # Step 7: Read and convert the data from the CSV log file to a DataFrame
+            log_data = read_csv_file(subfolder + log_filename + ".csv")
+            log_data["LATTICE_DSGN_ABSORPTION_BLUE"] = absorption_blue_value
+            log_data["LATTICE_DSGN_SCATTER_WHITE"] = scatter_white_value
+            quantities_of_interest = [
+                float(log_data["Cur_outflow"]),
+                float(log_data["Total_outflow"]),
+                float(log_data["Cur_absorption"]),
+                float(log_data["Total_absorption"]),
+                float(log_data["Max_absorption"]),
+                float(log_data["Wall_time_[s]"]),
+            ]
+    else:
+        quantities_of_interest = [0] * 7
+
+    return [quantities_of_interest]
 
 
 if __name__ == "__main__":

src/scraping_utils.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 import os
+import re
+
 
 def read_csv_file(csv_out_file):
     data_dict = {}
@@ -13,7 +15,7 @@ def read_csv_file(csv_out_file):
     with open(csv_out_file, "r") as file:
         csv_reader = csv.reader(file)
 
-        try:         # Read the header row
+        try:  # Read the header row
             header_row = next(csv_reader)
         except StopIteration:
             raise ValueError(f"File {csv_out_file} is empty")
@@ -46,7 +48,10 @@ def read_csv_file(csv_out_file):
     return data_dict
 
 
-def get_integrated_hohraum_probe_moments(csv_out_file, N=10):
+def get_integrated_hohraum_probe_moments(unique_name, t_final, N=10):
+    dt = extract_time_step(unique_name)
+
+    csv_out_file = unique_name + ".csv"
     df = pd.read_csv(csv_out_file)
     # Specify the column names to be averaged
     column_names = [
@@ -68,23 +73,47 @@ def get_integrated_hohraum_probe_moments(csv_out_file, N=10):
     # N = 10  # Replace with the desired number of intervals
 
     # Compute the time averages
-    averages = time_average(df, column_names, N)
+    averages = time_average(df, column_names, N, t_final, dt)
     # print(averages)
     return averages
 
 
-def time_average(df, column_names, N):
+def time_average(df, column_names, N, t_final):
     averages = {}
-
+    dt = t_final / N
     for col in column_names:
         data = df[col].values
         interval_length = len(data) // N
 
         interval_means = [
-            np.mean(data[i * interval_length : (i + 1) * interval_length])
-            for i in range(N)
+            np.sum(data[i * interval_length : (i + 1) * interval_length]) * dt
+            for i in range(N - 1)
         ]
-
+        tmp = np.sum(data[N - 2 * interval_length : -2]) * dt + data[-1] * dt
+        interval_means.append(tmp)
         averages[col] = interval_means
 
     return averages
+
+
+def extract_time_step(log_file_path):
+    search_string = "Corresponding maximal time-step:"
+    float_value = None
+
+    with open(log_file_path, "r") as log_file:
+        for line in log_file:
+            if search_string in line:
+                # Use regex to find the float value after the search string
+                match = re.search(
+                    rf"{re.escape(search_string)}\s*([0-9]*\.?[0-9]+)", line
+                )
+                if match:
+                    float_value = float(match.group(1))
+                    break  # Assuming there's only one such line in the log file
+
+    if float_value is not None:
+        print(f"Extracted float value: {float_value}")
+    else:
+        print("No matching line found.")
+
+    return float_value